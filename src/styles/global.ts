2f4d7c8e901bd3ed261cacdbf3c624950698ea14
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-09-07 09:07:45 -0300

    configure colors and fonts default

---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+  }
+`
