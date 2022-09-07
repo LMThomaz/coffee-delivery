7fa2abd3b44ae0ccb88a93d497bb6307482ba959
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-09-07 11:46:13 -0300

    created Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
 import logoImg from '@assets/logo.png'
-import { ContainerHeader } from './styles'
+import { MapPin, ShoppingCart } from 'phosphor-react'
+import { CartShopping, ContainerHeader, InfoLocation } from './styles'
 
 export function Header() {
   return (
     <ContainerHeader>
       <img src={logoImg} />
+      <nav>
+        <InfoLocation>
+          <MapPin size={22} weight="fill" /> Taiaçu, SP
+        </InfoLocation>
+        <CartShopping>
+          <ShoppingCart size={22} weight="fill" />
+        </CartShopping>
+      </nav>
     </ContainerHeader>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,44 @@
 import styled from 'styled-components'
 
-export const ContainerHeader = styled.header``
+export const ContainerHeader = styled.header`
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 2rem 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  nav {
+    display: flex;
+    gap: 0.75rem;
+  }
+`
+
+export const BaseButton = styled.button`
+  border: none;
+  border-radius: 6px;
+  padding: 0.5rem;
+  line-height: 1.3;
+  font-size: 0.875rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25rem;
+`
+
+export const InfoLocation = styled(BaseButton)`
+  background-color: ${({ theme }) => theme['purple-200']};
+  color: ${({ theme }) => theme['purple-700']};
+`
+
+export const CartShopping = styled(BaseButton)`
+  background-color: ${({ theme }) => theme['yellow-200']};
+  color: ${({ theme }) => theme['yellow-700']};
+  cursor: pointer;
+  box-shadow: 0 0 0 0 ${({ theme }) => theme['yellow-700']};
+  transition: all 160ms ease-in-out;
+
+  &:hover {
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['yellow-700']};
+  }
+`
