8dabf2d31fdbf1c83dce770280ce264d69334d54
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-01-12 09:57:40 -0300

    Feature: Show quantity item in cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,13 @@
 import logoImg from '@assets/logo.png'
 import { MapPin, ShoppingCart } from 'phosphor-react'
 import { Link } from 'react-router-dom'
+import { useCart } from '../../contexts/CartContext'
 import { Badge } from '../Badge'
 import { CartShopping, ContainerHeader, InfoLocation } from './styles'
 
-interface Props {
-  quantityItemsCard?: number
-}
+export function Header() {
+  const { quantityItemsInCart } = useCart()
 
-export function Header({ quantityItemsCard = 10 }: Props) {
   return (
     <ContainerHeader>
       <Link to="/">
@@ -19,7 +18,7 @@ export function Header({ quantityItemsCard = 10 }: Props) {
           <MapPin size={22} weight="fill" /> Taiaçu, SP
         </InfoLocation>
         <CartShopping to="/checkout">
-          <Badge quantity={quantityItemsCard} />
+          <Badge quantity={quantityItemsInCart} />
           <ShoppingCart size={22} weight="fill" />
         </CartShopping>
       </nav>
